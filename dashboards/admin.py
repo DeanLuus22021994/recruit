a53ea5e9021a4a413b32dd338e7d3e79470c7925
DeanLuus22021994 <dean.luus22021994@gmail.com>
2025-06-15 08:36:56 +0200

    refactor: remove unused import statements in admin and tests modules

--- dashboards/admin.py
+++ dashboards/admin.py
@@ -1,5 +1,3 @@
 """Django admin configuration for dashboards app."""
 
-from django.contrib import admin
-
 # Register your models here.
--- dashboards/tests.py
+++ dashboards/tests.py
@@ -1,3 +1,3 @@
-from django.test import TestCase
+"""Tests for the dashboards app."""
 
 # Create your tests here.
